bede67e36221490b3b1f5584e83ecb88062c4472
Abraham E. Tavarez <abetavarez@icloud.com>
2020-12-30 21:49:46 -0500

    regex

--- Automations/regex/escaping_chars.py
+++ Automations/regex/escaping_chars.py
@@ -6,3 +6,8 @@ print(re.search(r'.com', 'welcome'))
 print(re.search(r'\.com', 'welcome'))
 # escaping the .
 print(re.search(r'\.com', 'mydomain.com'))
+
+# using \w*
+print(re.search(r'\w*', 'This is an example'))
+# using \w*
+print(re.search(r'\w*', 'And_this_is_another'))
