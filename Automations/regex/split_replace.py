eb17c6baf644e39f8005a2959a97582f0ae4402b
Abraham E. Tavarez <abetavarez@icloud.com>
2021-01-02 23:34:50 -0500

    sub func

--- Automations/regex/split_replace.py
+++ Automations/regex/split_replace.py
@@ -13,3 +13,12 @@ print(re.split(r'([.?!])', re_str))
 # * Sub
 re_str1 = 'Received an email for go_nuts95@my.exmaple.com'
 print(re.sub(r'[\w.%+-]+@[\w.-]+', '[REDACTED FROM TEXT]', re_str1))
+
+# Using capturing groups to reverse name
+print(re.sub(r'^([\w .-]*), ([\w .-]*)$', r'\2 \1', 'Lovelace, Ada'))
+
+
+# We want to split a piece of text by either the word "a" or "the", as implemented in the following code.What is the resulting split list?
+
+print(re.split(r"the|a", "One sentence. Another one? And the last one!"))
+#['One sentence. Ano', 'r one? And ', ' l', 'st one!']
