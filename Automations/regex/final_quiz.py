deb69ca7561453c1db3b480a2fd9cf0c696978c9
Abraham E. Tavarez <abetavarez@icloud.com>
2021-01-03 20:34:16 -0500

    phone record split

---
+++ Automations/regex/final_quiz.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import re
+
+# * Question 1
+# We're working with a CSV file, which contains employee information. Each record has a name field, followed by a phone number field, and a role field. The phone number field contains U.S. phone numbers, and needs to be modified to the international format, with "+1-" in front of the phone number. Fill in the regular expression, using groups, to use the transform_record function to do that.
+
+
+def transform_record(record):
+    new_record = re.sub(r'([\d-]+)', r'+1-\1', record)
+    return new_record
+
+
+print(transform_record("Sabrina Green,802-867-5309,System Administrator"))
+# Sabrina Green,+1-802-867-5309,System Administrator
+
+print(transform_record("Eli Jones,684-3481127,IT specialist"))
+# Eli Jones,+1-684-3481127,IT specialist
+
+print(transform_record("Melody Daniels,846-687-7436,Programmer"))
+# Melody Daniels,+1-846-687-7436,Programmer
+
+print(transform_record("Charlie Rivera,698-746-3357,Web Developer"))
+# Charlie Rivera,+1-698-746-3357,Web Developer
