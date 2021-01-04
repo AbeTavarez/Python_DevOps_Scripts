565608e270e63ba06195e8fe72e87c234f39c2e3
Abraham E. Tavarez <abetavarez@icloud.com>
2021-01-03 22:55:17 -0500

    convert phone num

--- Automations/regex/final_quiz.py
+++ Automations/regex/final_quiz.py
@@ -66,3 +66,25 @@ print(transform_comments("  number += 1   # Increment the variable"))
 # Should be "  number += 1   // Increment the variable"
 print(transform_comments("  return(number)"))
 # Should be "  return(number)"
+
+
+# * Question 5
+# The convert_phone_number function checks for a U.S. phone number format: XXX-XXX-XXXX (3 digits followed by a dash, 3 more digits followed by a dash, and 4 digits), and converts it to a more formal format that looks like this: (XXX) XXX-XXXX. Fill in the regular expression to complete this function.
+
+def convert_phone_number(phone):
+    result = re.sub(r'^([A-Za-z ]+),\b(\d{3})\b', r'(\2)', phone)
+    return result
+
+
+# My number is (212) 345-9999.
+print(convert_phone_number("My number is 212-345-9999."))
+# Please call (888) 555-1234
+print(convert_phone_number("Please call 888-555-1234"))
+print(convert_phone_number("123-123-12345"))  # 123-123-12345
+# Phone number of Buckingham Palace is +44 303 123 7300
+print(convert_phone_number("Phone number of Buckingham Palace is +44 303 123 7300"))
+
+# Not quite. How did you define the capturing groups that are
+# used to change the format of the passed text? Did you
+# remember the escape characters for digits and word
+# boundaries?
