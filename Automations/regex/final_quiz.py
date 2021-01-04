46e10f96c2793193571367ab4a7d8bd7ca58a967
Abraham E. Tavarez <abetavarez@icloud.com>
2021-01-03 21:08:03 -0500

    parsing # to //

--- Automations/regex/final_quiz.py
+++ Automations/regex/final_quiz.py
@@ -48,3 +48,21 @@ print(multi_vowel_words("The order of a data queue is First In First Out (FIFO)"
 
 print(multi_vowel_words("Hello world!"))
 # []
+
+
+# *Question 4
+# The transform_comments function converts comments in a Python script into those usable by a C compiler. This means looking for text that begins with a hash mark (#) and replacing it with double slashes (//), which is the C single-line comment indicator. For the purpose of this exercise, we'll ignore the possibility of a hash mark embedded inside of a Python command, and assume that it's only used to indicate a comment. We also want to treat repetitive hash marks (##), (###), etc., as a single comment indicator, to be replaced with just (//) and not (#//) or (//#). Fill in the parameters of the substitution method to complete this function:
+
+def transform_comments(line_of_code):
+    result = re.sub(r'([\#])+', r'//', line_of_code)
+    return result
+
+
+print(transform_comments("### Start of program"))
+# Should be "// Start of program"
+print(transform_comments("  number = 0   ## Initialize the variable"))
+# Should be "  number = 0   // Initialize the variable"
+print(transform_comments("  number += 1   # Increment the variable"))
+# Should be "  number += 1   // Increment the variable"
+print(transform_comments("  return(number)"))
+# Should be "  return(number)"
