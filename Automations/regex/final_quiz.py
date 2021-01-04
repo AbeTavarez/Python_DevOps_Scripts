8bacd17f7976f98a7e32c2b66df2f06637d8663a
Abraham E. Tavarez <abetavarez@icloud.com>
2021-01-03 21:01:20 -0500

    consecutive vowels

--- Automations/regex/final_quiz.py
+++ Automations/regex/final_quiz.py
@@ -22,3 +22,29 @@ print(transform_record("Melody Daniels,846-687-7436,Programmer"))
 
 print(transform_record("Charlie Rivera,698-746-3357,Web Developer"))
 # Charlie Rivera,+1-698-746-3357,Web Developer
+
+
+# * Question 2
+# The multi_vowel_words function returns all words with 3 or more consecutive vowels (a, e, i, o, u). Fill in the regular expression to do that.
+
+def multi_vowel_words(text):
+    pattern = r'\b(?=[a-z]*[aeiou]{3,})[a-z]+\b'
+    result = re.findall(pattern, text, re.IGNORECASE)
+    return result
+
+
+print(multi_vowel_words("Life is beautiful"))
+# ['beautiful']
+
+print(multi_vowel_words("Obviously, the queen is courageous and gracious."))
+# ['Obviously', 'queen', 'courageous', 'gracious']
+
+print(multi_vowel_words(
+    "The rambunctious children had to sit quietly and await their delicious dinner."))
+# ['rambunctious', 'quietly', 'delicious']
+
+print(multi_vowel_words("The order of a data queue is First In First Out (FIFO)"))
+# ['queue']
+
+print(multi_vowel_words("Hello world!"))
+# []
