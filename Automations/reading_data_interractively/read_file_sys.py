87b66741f0cbec2168e4286ec3f33ffa332d8f13
Abraham E. Tavarez <abetavarez@icloud.com>
2021-01-05 13:19:48 -0500

    CRON logs

--- Automations/reading_data_interractively/read_file_sys.py
+++ Automations/reading_data_interractively/read_file_sys.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
-
+"Read Log file and Search for CRON lines"
 import sys
+import re
 
 if sys.argv[1]:
     logfile = sys.argv[1]
 else:
     exit(1)
+
 with open(logfile) as f:
     for line in f:
-        print(line.strip())
+        if 'CRON' not in line:
+            continue
+        pattern = r'USER \((\w+)\)$'
+        result = re.search(pattern, line)
+        print('Found --->', line.strip())
