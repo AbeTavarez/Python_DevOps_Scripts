8c42154d890e786cdcf07976178236de0da7a094
Abraham E. Tavarez <abetavarez@icloud.com>
2021-01-14 22:21:37 -0500

    creating a module comments and samples

--- creating_a_module/first_module.py
+++ creating_a_module/first_module.py
@@ -1,6 +1,10 @@
 
 import math
 
+# * Let's encapsulate some code in this file
+# * Here we declared some functions to be use outside this file
+# * Use these functions on the script.py file
+
 # *** Calculate areas *** #
 
 # * Triangle area
