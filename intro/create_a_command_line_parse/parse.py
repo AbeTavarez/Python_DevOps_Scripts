f5dc15eb6134ec9ebff1cafbd0f1e801970c1b83
Abraham E. Tavarez <abetavarez@icloud.com>
2021-01-16 13:29:58 -0500

    command line parser

---
+++ intro/create_a_command_line_parse/parse.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+# We need the sys module
+import sys
+
+
+def user_greeting(greeting, name):
+    message = f'{greeting} {name}'
+    print(message)
+
+
+if __name__ == '__main__':
+    greeting = 'Hello'
+    name = 'User'
+
+    if '--help' in sys.argv:
+        help_message = f'Usage: {sys.argv[0]} --name <NAME> --greeting <GREETING>'
+        print(help_message)
+        sys.exit()
+
+    if '--name' in sys.argv:
+        # get position after name flag
+        name_index = sys.argv.index('--name') + 1
+        if name_index < len(sys.argv):
+            name = sys.argv[name_index]
+
+    if '--greeting' in sys.argv:
+        # get position after greeting flag
+        greeting_index = sys.argv.index('--greeting') + 1
+        if greeting_index < len(sys.argv):
+            greeting = sys.argv[greeting_index]
+
+user_greeting(greeting, name)
+
+# * First make the script executable
+# * Now you can run the script:
+# ./parse.py --greeting 'Welcome to the Matrix' --name "Neo"
