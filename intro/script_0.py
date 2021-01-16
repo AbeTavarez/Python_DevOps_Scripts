93e655a0ca9ec32b178c0c83c3be374887df83bc
Abraham E. Tavarez <abetavarez@icloud.com>
2021-01-15 22:19:32 -0500

    intro comments

--- intro/script_0.py
+++ intro/script_0.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3
+
+# *************** Making s script executable *******************
+
+# * Notice how we set the interpreter at the top of the file to python3
+#! using -> #!/usr/bin/env python3   <- on all files
+
+
+# * Then write your code after:
+
 print("Running script...")
