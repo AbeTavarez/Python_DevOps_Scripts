28377fdbc0d9534a90320c88f9ad77d99b83425a
Abraham E. Tavarez <abetavarez@icloud.com>
2021-01-18 23:14:06 -0500

    using click

--- intro/create_a_command_line_parse/using_click.py
+++ intro/create_a_command_line_parse/using_click.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 
 import click
+
+
+@click.command()
+@click.option('--greeting', default='Hello', help='Who do you want to greet?')
+@click.option('--name', default='Tammy', help='Who do you want to greet?')
+def greet(greeting, name):
+    print(f"{greeting} {name}")
+
+
+if __name__ == '__main__':
+    greet()
