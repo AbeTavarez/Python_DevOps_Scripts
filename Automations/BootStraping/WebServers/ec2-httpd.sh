eb95e2825df4fca5199810963c03ba15079ac403
Abraham E. Tavarez <abeincwireless@gmail.com>
2021-02-07 20:56:56 -0500

    apache installation on start script

---
+++ Automations/BootStraping/WebServers/ec2-httpd.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#==========================================
+# INSTALL APACHE SERVER ON AMAZON LINUX 2
+#==========================================
+
+# Get super user priviliges
+sudo su
+
+#=== Update and Install httpd (Linux 2 Version) ===
+
+# Updates instance (server)
+yum update -y 
+# Install httpd.x86_64
+yum install -y httpd.x86_64
+# Start httpd service
+systemctl start httpd.service
+# Enables httpd service auto-restart if instance gets re-started
+systemctl enable http.service
+# -OPTIONAL: Overwrites apache welcome index.html file with instance name
+echo "$(hostname -f) is now running..." > /var/www/html/index.html 
